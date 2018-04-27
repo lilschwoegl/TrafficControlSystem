d4378a4d09052820efd7350661dce21912645cf5
Michael Schwoegl <lilschwoegl@hotmail.com>
2018-04-27 14:30:11 -0400

    added code for missing tests

--- src/Test/Initial_Main_Test.java
+++ src/Test/Initial_Main_Test.java
@@ -5,31 +5,96 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Instant;
 import java.util.Date;
+import java.util.Vector;
 
 import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
 import application.BulbColor;
+import application.DetectedObject;
 import application.SQLite;
 import application.TrafficController;
 import application.TrafficController.SignalLogicConfiguration;
 import config.TrafficControllerConfig;
 import application.TrafficLight;
+import application.VideoInput;
+import application.YoloDetector;
 import simulator.Constants.Direction;
 
 public class Initial_Main_Test {
 	
 	@Test
 	public void getvideoFeedTest () {
-		fail("Not yet implemented");
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		VideoInput vi = new VideoInput();
+		vi.selectCameraFeed("Recorded 1");
+		
+		if (!vi.isOpened())
+			fail("Video feed not opened");
+		
+		Mat m = new Mat();
+		try {
+			vi.grabFrame().copyTo(m);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue("Video feed connected and frame processed", m.width() > 0 && m.height() > 0);
 	}
 	
 	@Test
 	public void incomingVehicleTest () {
-		fail("Not yet implemented");
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		VideoInput vi = new VideoInput();
+		YoloDetector yolo = null;
+		try {
+			yolo = new YoloDetector("yolo/cfg/yolov2-custom.cfg",
+					"yolo/weights/yolov2-tiny-obj_10500.weights",
+					"yolo/classes/custom-training.names");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			fail("YOLO network not created");
+		}
+		vi.selectCameraFeed("Recorded 1");
+		
+		if (!vi.isOpened())
+			fail("Video feed not opened");
+		
+		Mat m = new Mat();
+		boolean vehicleFound = false;
+		int maxCount = 100, count = 0;
+		Vector<DetectedObject> detects = new Vector<DetectedObject>();
+		
+		while (count < maxCount && !vehicleFound)
+		{
+		
+			try {
+				vi.grabFrame().copyTo(m);
+				detects = yolo.detectObjectYolo(m);
+				
+				for (DetectedObject d : detects)
+				{
+					if (d.getClassName().equals("Regular Vehicle"))
+					{
+						return;
+					}
+				}
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 	
 	@Test
